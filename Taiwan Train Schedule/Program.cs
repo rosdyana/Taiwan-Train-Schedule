62be932d7f1e5589076f381e7ba39679eab45cae
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2016-12-09 21:08:38 +0800

    - initial commit for Taiwan Train Schedule

---
+++ Taiwan Train Schedule/Program.cs
@@ -0,0 +1,117 @@
+﻿/**
+* Copyright (C) 2016 Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
+* 
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 3 of the License, or (at your option) any later
+* version.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+* details.
+* 
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, see <http://www.gnu.org/licenses/>.
+**/
+using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Taiwan_Train_Schedule
+{
+    class Options
+    {
+        [Option('F', "from", Required = true,
+            HelpText = "Input origin of train station.")]
+        public string fromStation { get; set; }
+
+        [Option('T',"to", Required = true,
+            HelpText = "Input destination of train station.")]
+        public string toStation { get; set; }
+
+        [Option('s',"start time", DefaultValue = "00:00",
+            HelpText = "Input start time.")]
+        public string startTime { get; set; }
+
+        [Option('e', "end time", DefaultValue = "23:59",
+            HelpText = "Input end time.")]
+        public string endTime { get; set; }
+
+        [Option('d',"date",
+            HelpText = "Input date , format yyyy/mm/dd, by default is today.")]
+        public string date { get; set; }
+
+        [Option('c',"class", DefaultValue = "2",
+            HelpText = "Input the car class  0 = express , 1 = ordinary , 2 = all types.")]
+        public string carClass { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> NorthStationList = new HashSet<string>(new string[]
+            {
+                "Neili,1016", "Taipei,1008"
+            });
+            WebRequest req = WebRequest.Create("http://163.29.3.97/mobile_en/result2.jsp");
+            TimeZone zone = TimeZone.CurrentTimeZone;
+            DateTime local = zone.ToLocalTime(DateTime.Now);
+
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                if (options.date == null) options.date = local.ToString("yyyy/MM/dd");
+                if (options.startTime != null) options.startTime = local.ToString("hh:mm");
+                foreach (string x in NorthStationList)
+                {
+                    var j = x.Split(',');
+                    if (options.fromStation.IndexOf(j[0], StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        options.fromStation = j[1];
+                    }
+                    else if (options.toStation.IndexOf(j[0], StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        options.toStation = j[1];
+                    }
+                }
+                string postData = "from1=" + options.fromStation + "&to1=" + options.toStation + "&carclass=" + options.carClass + "&Date=" + options.date + "&sTime=" + options.startTime + "&eTime=" + options.endTime + "&Submit=Submit";
+
+                byte[] send = Encoding.Default.GetBytes(postData);
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = send.Length;
+
+                Stream sout = req.GetRequestStream();
+                sout.Write(send, 0, send.Length);
+                sout.Flush();
+                sout.Close();
+
+                WebResponse res = req.GetResponse();
+                StreamReader sr = new StreamReader(res.GetResponseStream());
+                string returnvalue = sr.ReadToEnd();
+                var fileName = Guid.NewGuid() + ".html";
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName, true))
+                {
+                    file.WriteLine(returnvalue);
+                }
+                System.Diagnostics.Process.Start(fileName);
+            }
+            
+        }
+    }
+}
---
+++ Taiwan Train Schedule/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Taiwan Train Schedule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rosdyana Kusuma <rosdyana.kusuma@gmail.com>")]
+[assembly: AssemblyProduct("Taiwan Train Schedule")]
+[assembly: AssemblyCopyright("Copyright © 2016 Rosdyana Kusuma <rosdyana.kusuma@gmail.com>")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("483219e7-bbc3-469c-bcf1-17e4ac842c61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
